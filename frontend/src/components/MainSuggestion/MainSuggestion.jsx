99b4eee98a9c1f4558c22b13a53746ab7866693f
kishan kumar <rajkishan7870@gmail.com>
2024-07-10 03:03:31 +0530

    some changes in suggestion page

--- frontend/src/components/MainSuggestion/MainSuggestion.jsx
+++ frontend/src/components/MainSuggestion/MainSuggestion.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import style from "./MainSuggestion.module.css"
+import SuggestionCard from './SuggestionCard'
 
 export default function MainSuggestion() {
   return (
       <div className={style.parentDiv}>
-          
+          <SuggestionCard/>
     </div>
   )
 }
---
+++ frontend/src/components/MainSuggestion/SuggestionCard.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function SuggestionCard() {
+  return (
+    <div>SuggestionCard</div>
+  )
+}
