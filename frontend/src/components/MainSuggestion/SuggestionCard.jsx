808f7f20d4cd6f656588fa083bad9a79e749bebc
kishan kumar <rajkishan7870@gmail.com>
2024-07-10 18:02:54 +0530

    suggestion page done

--- frontend/src/components/MainSuggestion/SuggestionCard.jsx
+++ frontend/src/components/MainSuggestion/SuggestionCard.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
+import style from "./SuggestionCard.module.css"
 
 export default function SuggestionCard() {
-  return (
-    <div>SuggestionCard</div>
-  )
+    return (
+      <CarouselProvider
+        naturalSlideWidth={200}
+        naturalSlideHeight={225}
+        totalSlides={3}
+      >
+        <Slider>
+          <Slide index={0}>I am the first Slide.</Slide>
+          <Slide index={1}>I am the second Slide.</Slide>
+          <Slide index={2}>I am the third Slide.</Slide>
+        </Slider>
+        <ButtonBack className={style.leftButton}><FaAngleLeft /></ButtonBack>
+        <ButtonNext className={style.rightButton}><FaAngleRight /></ButtonNext>
+      </CarouselProvider>
+    );
 }
